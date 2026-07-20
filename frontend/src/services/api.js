682580ea0a7c684a09076ea7f82189003158b157
ArielNicolas2021 <ariel.nicolas.aguilar@gmail.com>
2026-07-20 11:42:02 -0300

    Update link to database

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,1 @@
-const API_URL = "http://3.131.13.187:8080/api";
-
-export const apiFetch = async (endpoint, options = {}) => {
-  const response = await fetch(`${API_URL}${endpoint}`, {
-    headers: { "Content-Type": "application/json" },
-    ...options,
-  });
-  return response.json();
-};
+const API_URL = "http://localhost:8080/api";
